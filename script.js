7a92bc146c97a6766d9d15708cf9237bd0c13085
ryotn <kh2oukusyon@gmail.com>
2021-10-26 16:00:11 +0900

    プレミア公開の非表示に対応

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const SUBSCRIPTIONS_URL = "https://www.youtube.com/feed/subscriptions";
+const PREMIERE_DISPLAY_TIME = 5 * 60 * 60 * 1000;
 let contents = null;
 let subscriptions = null;
 
@@ -25,18 +26,28 @@ function hiddenShorts() {
         let videoTime = gridElement.querySelector('span.style-scope.ytd-thumbnail-overlay-time-status-renderer');
         if (videoTime == undefined) {
             //console.log("videoTime == undefined gridElement:" + gridElement.innerText);
+            let badge = gridElement.querySelector('span.style-scope.ytd-badge-supported-renderer');
+            if (badge != undefined && badge.innerText.trim() == "プレミア公開中") gridElement.classList.add("notShortVideo");
             return;
         }
 
-        let splitTime = videoTime.innerText.trim().split(":");
+        let strTime = videoTime.innerText.trim();
+        let splitTime = strTime.split(":");
         let time = 0;
-        if (splitTime.length < 3) {
+        if (splitTime.length == 2) {
             time = splitTime[0] * 60;
             time += splitTime[1] * 1;
-        } else {
+        } else if (splitTime.length >= 3) {
             time = splitTime[0] * 60 * 60;
             time += splitTime[1] * 60;
             time += splitTime[2] * 1;
+        } else if (strTime == "プレミア公開") {
+            //gridElement.classList.add("premiereVideo");
+            let strPublishingTime = gridElement.querySelector('div#metadata-line').innerText.trim().slice(0, -7);
+            let diffPublishingTime = new Date(strPublishingTime + ":00").getTime() - new Date().getTime();
+            //console.log("strPublishingTime:" + strPublishingTime + " diffPublishingTime:" + diffPublishingTime);
+            if (diffPublishingTime > PREMIERE_DISPLAY_TIME) gridElement.classList.add("hiddenPremiereVideo");
+            time = 62;
         }
 
         if (time <= 61) {
@@ -70,9 +81,9 @@ function setContentsObserver() {
 
         subscriptionsObserver.disconnect();
         subscriptionsObserver = new MutationObserver(function () {
-            if(subscriptions.hidden){
+            if (subscriptions.hidden) {
                 restoreAll();
-            }else{
+            } else {
                 setHiddenShortsTimer();
             }
         });
@@ -89,7 +100,7 @@ function setContentsObserver() {
     }
 }
 
-function restoreAll(){
+function restoreAll() {
     let videoGrids = contents.querySelectorAll('.notShortVideo,.hiddenShortVideo');
     if (videoGrids.length < 1) return;
     videoGrids = Array.from(videoGrids);
@@ -101,7 +112,7 @@ function restoreAll(){
 window.addEventListener('load', function () {
 
     let newStyleElement = document.createElement("style");
-    newStyleElement.innerHTML = ".hiddenShortVideo{display:none !important;}";
+    newStyleElement.innerHTML = ".hiddenShortVideo{display:none !important;}.hiddenPremiereVideo{display:none !important;}";
     document.head.appendChild(newStyleElement);
 
     pageManagerObserver = new MutationObserver(function () {
